dafb82d86891ba3fe80d44111e21ba1f56998eb0
DavVel5 <david.veliz2@unmsm.edu.pe>
2024-04-16 16:13:29 -0500

    cambio1

--- proyecto-tad-g4/server/index.js
+++ proyecto-tad-g4/server/index.js
@@ -25,7 +25,7 @@ app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-      return res.status(400).json({ message: 'Por favor , proporciona un nombre de usuario y una contraseña xddddddd.' });
+      return res.status(400).json({ message: 'Por favor , proporciona un nombre de dav' });
   }
 
   db.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
