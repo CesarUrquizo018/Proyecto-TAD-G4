a315c16580282f8539b911852fe527363b453fb0
CesarUrquizo018 <cesar.urquizo@unmsm.edu.pe>
2024-05-05 17:03:25 -0500

    añadir hojas de estilo

--- proyecto-tad-g4/client/src/pages/CreateUserPage.js
+++ proyecto-tad-g4/client/src/pages/CreateUserPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../assets/styles/register.css';  // Asegúrate de que la ruta es correcta
+import '../assets/styles/register_user.css';  // Asegúrate de que la ruta es correcta
 
 function CreateUserPage() {
     const [username, setUsername] = useState('');
--- proyecto-tad-g4/client/src/pages/UserPage.js
+++ proyecto-tad-g4/client/src/pages/UserPage.js
@@ -1,5 +1,4 @@
 // src/pages/UserPage.js
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useUser } from '../context/UserContext';
@@ -19,85 +18,18 @@ function UserPage() {
       <div>
         <p>No se ha iniciado sesión. Por favor, vuelve a la página de inicio de sesión.</p>
         <Link to="/login">Ir a Iniciar Sesión</Link>
-=======
-import '../assets/styles/user_page.css'; // Ajusta la ruta si es necesario
-import { useUser } from '../contexto/UserContext';
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
-
-  function UserPage() {
-    const { user } = useUser();// Acceder al objeto usuario desde el contexto
-    const [showPassword, setShowPassword] = useState(false); // Estado para controlar si se muestra la contraseña
-    
-    const toggleShowPassword = () => {
-      setShowPassword(!showPassword);
-    };
-
-    return (
-      <div>
-        <div className="enlaces">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/user">User</Link>
-              </li>
-              <li>
-                <Link to="/myproyect">Mis Proyectos</Link>
-              </li>
-              <li>
-              <Link to="/">Salir</Link>
-            </li>
-            </ul>
-          </nav>
-        </div>
-  
-        <div className="user-container">
-          <h1>Bienvenido, {user.nombre}!</h1>
-          <div>
-            <label className="user-info-label">ID:</label>
-            <span className="user-info-value">{user.id_usuario}</span>
-          </div>
-          <div>
-            <label className="user-info-label">Código:</label>
-            <span className="user-info-value">{user.codigo}</span>
-          </div>
-          <div>
-            <label className="user-info-label">Email:</label>
-            <span className="user-info-value">{user.email}</span>
-          </div>
-          <div>
-            <label className="user-info-label">Contraseña:</label>
-            {showPassword ? (
-              <span className="user-info-value">{user.contrasena}</span>
-            ) : (
-              <span className="password-mask">********</span>
-            )}
-            <button onClick={toggleShowPassword}>{showPassword ? 'Ocultar' : 'Mostrar'}</button>
-          </div>
-        </div>
->>>>>>> 8de5989055a15137c863151da87a4abeeabb3449
       </div>
     );
   }
 
-<<<<<<< HEAD
   return (
     <div>
       <div className="enlaces">
         <nav>
           <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/user">User</Link>
-            </li>
-            <li>
-              <Link to="/">Salir</Link>
-            </li>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/user">User</Link></li>
+            <li><Link to="/">Salir</Link></li>
           </ul>
         </nav>
       </div>
@@ -130,6 +62,4 @@ import React, { useState } from 'react';
   );
 }
 
-=======
->>>>>>> 8de5989055a15137c863151da87a4abeeabb3449
 export default UserPage;
